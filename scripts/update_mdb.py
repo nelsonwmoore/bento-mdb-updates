a3b0ef27e6001e99235b8bd0e80165fd20df5f85
Nelson Moore <nelsonwmoore@gmail.com>
2025-04-22 15:12:55 -0400

    feat(update_mdb.py): use prefect secret for pwd

--- scripts/update_mdb.py
+++ scripts/update_mdb.py
@@ -11,22 +11,23 @@ from pathlib import Path
 import click
 from dotenv import load_dotenv
 from prefect import flow, task
+from prefect.blocks.system import Secret
 from pyliquibase import Pyliquibase
 
 load_dotenv(override=True, dotenv_path="config/.env")
 logger = logging.getLogger(__name__)
 
 
+@task
 def set_defaults_file(
     mdb_uri: str,
     mdb_user: str,
-    mdb_pass: str,
     changelog_file: str,
 ) -> Path:
     """Create temporary defaults file and return path."""
     uri = mdb_uri or os.environ.get("NEO4J_MDB_URI")
     user = mdb_user or os.environ.get("NEO4J_MDB_USER")
-    password = mdb_pass or os.environ.get("NEO4J_MDB_PASS")
+    password = Secret.load("fnl-mdb-dev-pwd").get()
     with tempfile.NamedTemporaryFile(mode="w", delete=False) as f:
         f.write(f"changelogFile: {changelog_file}\n")
         f.write(f"url: {uri}\n")
@@ -58,13 +59,12 @@ def run_liquibase_update(defaults_file: Path | str, *, dry_run: bool = False) ->
 def liquibase_update_flow(
     mdb_uri: str,
     mdb_user: str,
-    mdb_pass: str,
     changelog_file: str,
     *,
     dry_run: bool = False,
 ) -> None:
     """Run Liquibase Update on Changelog."""
-    defaults_file = set_defaults_file(mdb_uri, mdb_user, mdb_pass, changelog_file)
+    defaults_file = set_defaults_file(mdb_uri, mdb_user, changelog_file)
     try:
         run_liquibase_update(defaults_file, dry_run=dry_run)
     finally:
@@ -86,14 +86,6 @@ def liquibase_update_flow(
     help="metamodel database username",
 )
 @click.option(
-    "--mdb_pass",
-    required=True,
-    type=str,
-    prompt=True,
-    help="metamodel database password",
-    hide_input=True,
-)
-@click.option(
     "--changelog_file",
     required=True,
     type=str,
@@ -109,7 +101,6 @@ def liquibase_update_flow(
 def main(
     mdb_uri: str,
     mdb_user: str,
-    mdb_pass: str,
     changelog_file: str,
     *,
     dry_run: bool = False,
@@ -118,7 +109,6 @@ def main(
     liquibase_update_flow(
         mdb_uri=mdb_uri,
         mdb_user=mdb_user,
-        mdb_pass=mdb_pass,
         changelog_file=changelog_file,
         dry_run=dry_run,
     )
