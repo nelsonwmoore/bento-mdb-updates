7146f4866711cdc9bb2675463ee0aaffbc01519a
Nelson Moore <nelsonwmoore@gmail.com>
2025-05-07 11:47:08 -0400

    test(docker): remove for now

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -2,14 +2,9 @@
 
 from pathlib import Path
 
-import pytest
-from bento_meta.mdb.mdb import MDB
-
 PROJECT_ROOT = Path(__file__).parent.parent
 DOCKERFILE_PATH = PROJECT_ROOT / "Dockerfile"
 DOCKER_IMAGE_TAG = "mdb-update-test:latest"
-TEST_MDB_USER = "neo4j"
-TEST_MDB_PASSWORD = "neo4j1"
 
 
 # @pytest.mark.docker
@@ -19,16 +14,16 @@ TEST_MDB_PASSWORD = "neo4j1"
 #     mdb = MDB(uri=bolt_url, user=TEST_MDB_USER, password=TEST_MDB_PASSWORD)
 #     assert mdb
 
-@pytest.mark.docker
-def test_prefect_flow(mdb_versioned, run_prefect_flow):
-    """Test that the Prefect flow is running."""
-    bolt_url, http_url = mdb_versioned
-    result = run_prefect_flow(
-        "tests/samples/test_prefect_flow.py",
-        "test-prefect-flow",
-        uri=bolt_url,
-        username=TEST_MDB_USER,
-        password=TEST_MDB_PASSWORD,
-    )
-    print(result)
-    assert result.returncode == 0
+# @pytest.mark.docker
+# def test_prefect_flow(mdb_versioned, run_prefect_flow):
+#     """Test that the Prefect flow is running."""
+#     bolt_url, http_url = mdb_versioned
+#     result = run_prefect_flow(
+#         "tests/samples/test_prefect_flow.py",
+#         "test-prefect-flow",
+#         uri=bolt_url,
+#         username=TEST_MDB_USER,
+#         password=TEST_MDB_PASSWORD,
+#     )
+#     print(result)
+#     assert result.returncode == 0
